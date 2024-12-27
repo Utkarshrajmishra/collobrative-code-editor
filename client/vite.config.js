cc540d58d12220150845bb72eac3658d03a458fd
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-12-27 22:48:38 +0530

    Add: Vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensures unmatched routes fall back to index.html
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
