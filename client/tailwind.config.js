a8a497d7af352966b31772ee58003658f042ccb8
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-12-27 21:51:24 +0530

    Fix: tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			inter: [
-  				'Inter',
-  				'sans-serif'
-  			]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
 
