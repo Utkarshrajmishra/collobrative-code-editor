24a10d0cfbfc3247719a4c8a87a11a13f65af2d0
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2024-12-28 09:42:12 +0530

    Update: Path

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
   const socket = useMemo(
-    () => io("https://collobrative-code-editor.onrender.com/"),
+    () => io("https://collobrative-code-editor.onrender.com"),
     []
   );
 
